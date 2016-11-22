a34af0e292ecfa768cb1befb47466da8cd480dc4
Leon Guo <chineseroyalstar@gmail.com>
2016-11-22 17:53:21 +0800

    Initial Commit

---
+++ GZLyricLabel/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GZLyricLabel
+//
+//  Created by armada on 2016/11/22.
+//  Copyright © 2016年 com.zlot.gz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GZLyricLabel/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GZLyricLabel
+//
+//  Created by armada on 2016/11/22.
+//  Copyright © 2016年 com.zlot.gz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
